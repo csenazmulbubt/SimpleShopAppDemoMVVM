25330e7ccdccbe020af065d8498d391dace60dde
csenazmulbubt <cse.nazmul.bubt@gmail.com>
2023-04-27 16:38:18 +0600

    [Add] create architecture folder structure

---
+++ SampleShopAppMVVM/Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SampleShopAppMVVM
+//
+//  Created by Nazmul on 27/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
