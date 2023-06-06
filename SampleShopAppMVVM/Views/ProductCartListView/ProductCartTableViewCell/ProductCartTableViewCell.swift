ed32e4ba2e4614e88ab6e133ee3807ba853ecb13
csenazmulbubt <cse.nazmul.bubt@gmail.com>
2023-06-07 01:03:52 +0600

    [Edit] update product list discount percentage and add productcartlist view

--- SampleShopAppMVVM/Views/ProductCartListView/ProductCartTableViewCell/ProductCartTableViewCell.swift
+++ SampleShopAppMVVM/Views/ProductCartListView/ProductCartTableViewCell/ProductCartTableViewCell.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 class ProductCartTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var productImageCV: UIView!
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    @IBOutlet weak var currentPriceLabel: UILabel!
+    
+    @IBOutlet weak var previousPriceLabel: UILabel!
+    
+    @IBOutlet weak var decreamentButton: UIButton!
+    
+    @IBOutlet weak var totalProductLabel: UILabel!
+    
+    @IBOutlet weak var incrementButton: UIButton!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -18,6 +33,15 @@ class ProductCartTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    
+    @IBAction func tappedOnDecreamentButton(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func tappedOnIncrementButton(_ sender: UIButton) {
     }
     
 }
